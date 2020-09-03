05d99bbc47600c8b7d8cd5a025626fc5e16dbe74
Israel <ipurizaga@indracompany.com>
2020-09-03 15:44:06 -0500

    first commit

---
+++ src/test/java/qaclickacademy/APITest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+		
+	}
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+		
+	}
+}
---
+++ src/test/java/qaclickacademy/Appium.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class Appium {
+	
+
+		@Test
+		public void nativeAppAndroid() {
+			System.out.println("nativeAppAndroid");
+		}
+		@Test
+		public void IOSApp() {
+			System.out.println("IOSApp");
+		}
+
+}
